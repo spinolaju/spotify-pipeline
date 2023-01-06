040fd75aa10c9b169383e813d04e354ca2dac598
Juliane Spinola <spinolaju@hotmail.com>
2023-01-06 14:11:00 +0000

    Updated queries

--- queries.py
+++ queries.py
@@ -29,9 +29,10 @@ engine = sqlalchemy.create_engine("mssql+pyodbc:///?odbc_connect={}".format(para
 
 
 
-id_query = """SELECT id, album_name FROM artist_album"""
+id_query = """SELECT id, album_name, total_tracks FROM artist_album"""
 album_ids = []
 album_duration = []
+album_loudness = []
 with engine.connect() as connection:
 
     result = connection.execute(id_query)
@@ -40,6 +41,7 @@ with engine.connect() as connection:
         temp = {}
         temp['album_id'] = str(r[0]).replace('(', '').replace(',', '').replace(')', '')
         temp['album_name'] = str(r[1]).replace('(', '').replace(',', '').replace(')', '')
+        temp['total_tracks'] = str(r[2]).replace('(', '').replace(',', '').replace(')', '')
         temp['duration_total'] = 0
         
         album_ids.append(temp)
@@ -53,6 +55,15 @@ with engine.connect() as connection:
             records['album_id'] = str(aid['album_id']).replace('(', '').replace(',', '').replace(')', '')
             records['duration_ms'] = str(dr).replace('(', '').replace(',', '').replace(')', '').replace("'", '')
             album_duration.append(records)
+
+    for loud in album_ids: 
+        album_loudness_query = f"SELECT loudness FROM track WHERE album_id = '{loud['album_id']}' "
+        loudness_res = connection.execute(album_loudness_query)
+        for lr in loudness_res:
+            records = {}
+            records['album_id'] = str(loud['album_id']).replace('(', '').replace(',', '').replace(')', '')
+            records['loudness'] = str(lr).replace('(', '').replace(',', '').replace(')', '').replace("'", '')
+            album_loudness.append(records)
 
 count = 0
 
@@ -73,11 +84,8 @@ print("""
 -----------------------------------------------------------------------------------------------------------------------------------
 """)
 
-
-#print(albums_duration_df.sort_values(by=['duration_total'], ascending=False).head(1).to_string(index=False))
-
 print("1. The longest album in minutes:")
-print(tabulate(albums_duration_df.sort_values(by=['duration_total']).head(1), showindex=False, headers=albums_duration_df.columns))
+print(tabulate(albums_duration_df.sort_values(by=['duration_total'], ascending=False).head(1), showindex=False, headers=albums_duration_df.columns))
 print()
 
 print("-----------------------------------------------------------------------------------------------------------------------------------")
@@ -89,3 +97,42 @@ print("-------------------------------------------------------------------------
 print("3. All albums listed from the longest to the shortest:")
 print(tabulate(albums_duration_df.sort_values(by=['duration_total'], ascending=False), showindex=False, headers=albums_duration_df.columns))
 
+
+
+for id in album_ids:
+   temp = 0
+   for ad in album_loudness:
+    
+     if ad['album_id'] == id['album_id']:
+        temp += float(ad['loudness'])
+      
+   id['loudness'] = temp / float(id['total_tracks'])
+
+album_loudness_df = pd.DataFrame(album_ids, columns = ["album_name", "loudness", "total_tracks", "album_id"])
+
+
+print("""
+-----------------------------------------------------------------------------------------------------------------------------------
+The overall loudness of album tracks in decibels (dB). Loudness values are averaged across the entire track and are useful for comparing
+relative loudness of tracks. Loudness is the quality of a sound that is the primary psychological correlate of physical strength 
+(amplitude). Values typically range between -60 and 0 db.
+-----------------------------------------------------------------------------------------------------------------------------------
+""")
+
+
+print("4. The average'loudest' album in decibels (dB):")
+print(tabulate(album_loudness_df.sort_values(by=['loudness'], ascending=False).head(1), showindex=False, headers=album_loudness_df.columns))
+
+print()
+
+print("-----------------------------------------------------------------------------------------------------------------------------------")
+print("5. The average 'quietest' album in decibels (dB):")
+print(tabulate(album_loudness_df.sort_values(by=['loudness'], ascending=False).tail(1), showindex=False, headers=album_loudness_df.columns))
+
+print()
+
+print("-----------------------------------------------------------------------------------------------------------------------------------")
+print("6. The overall loudness of albums in decibels (dB). All albums listed from the 'loudest' to the 'quietest':")
+print(tabulate(album_loudness_df.sort_values(by=['loudness'], ascending=False), showindex=False, headers=album_loudness_df.columns))
+
+  
